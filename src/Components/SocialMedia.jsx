45557570f1f8baf22c9b700afa5d40e47bb99792
Radoje <megasrbin1@gmail.com>
2023-03-08 15:09:35 +0100

    add new link for viber group

--- src/Components/SocialMedia.jsx
+++ src/Components/SocialMedia.jsx
@@ -1,51 +1,51 @@
 import { FaInstagram, FaViber, FaWhatsapp } from "react-icons/fa";
 
 const SocialMedia = () => {
-	return (
-		<>
-			<ul className="socialMedia">
-				<li className="socialMedia__item">
-					<a
-						href="https://www.instagram.com/rutanndelivery/"
-						target="_blank"
-						rel="noopener noreferrer"
-						aria-label="instagram icon"
-					>
-						<FaInstagram fill="#0a1931" size={24} />
-					</a>
-				</li>
-				<li className="socialMedia__item">
-					<a
-						href="https://wa.me/381695230188"
-						target="_blank"
-						rel="noopener noreferrer"
-						aria-label="Whatsapp icon"
-					>
-						<FaWhatsapp fill="#0a1931" size={24} />
-					</a>
-				</li>
-				{/* <li className="socialMedia__item"> */}
-				{/* 	<a */}
-				{/* 		href="viber://chat?number=+381695230188" */}
-				{/* 		rel="noopener noreferrer" */}
-				{/* 		aria-label="viber icon" */}
-				{/* 	> */}
-				{/* 		<FaViber fill="#0a1931" size={24} /> */}
-				{/* 	</a> */}
-				{/* </li> */}
-				<li className="socialMedia__item">
-					<a
-						href="https://invite.viber.com/?g2=AQBO1D5sWXXiHE3c7ujmoy3DFcHz1WjN7g6hRLx0Zn0va2LNSgNtDP8ZD3%2BGNWHk&fbclid=IwAR0xIV3Gp1GQa1tRDgK2f-NUf1T-81XL-4N97jCU6Jwf5A8iqcrYXC8Jc2w"
-						className="socialMedia__item--group"
-						aria-label="viber icon"
-					>
-						<p>Grupa</p>
-						<FaViber fill="#fff" size={24} />
-					</a>
-				</li>
-			</ul>
-		</>
-	);
+  return (
+    <>
+      <ul className="socialMedia">
+        <li className="socialMedia__item">
+          <a
+            href="https://www.instagram.com/rutanndelivery/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="instagram icon"
+          >
+            <FaInstagram fill="#0a1931" size={24} />
+          </a>
+        </li>
+        <li className="socialMedia__item">
+          <a
+            href="https://wa.me/381695230188"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Whatsapp icon"
+          >
+            <FaWhatsapp fill="#0a1931" size={24} />
+          </a>
+        </li>
+        {/* <li className="socialMedia__item"> */}
+        {/* 	<a */}
+        {/* 		href="viber://chat?number=+381695230188" */}
+        {/* 		rel="noopener noreferrer" */}
+        {/* 		aria-label="viber icon" */}
+        {/* 	> */}
+        {/* 		<FaViber fill="#0a1931" size={24} /> */}
+        {/* 	</a> */}
+        {/* </li> */}
+        <li className="socialMedia__item">
+          <a
+            href="https://invite.viber.com/?g2=AQAsOf7KxjMafUrgD4K4q%2BeKkr4n98Nu6UIZN1h%2FiEHjyp1gSp6k%2BU%2FfMA46c7RP&lang=en"
+            className="socialMedia__item--group"
+            aria-label="viber icon"
+          >
+            <p>Grupa</p>
+            <FaViber fill="#fff" size={24} />
+          </a>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default SocialMedia;
